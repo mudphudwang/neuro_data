2f1212c8ca05fa9c8346694657fd49e4cf002b54
Eric Wang <ewang@mantle.local>
2020-10-31 15:33:52 -0500

    ScanOracle

--- neuro_data/movies/data_schemas.py
+++ neuro_data/movies/data_schemas.py
@@ -7,8 +7,6 @@ from pprint import pformat
 import cv2
 import imageio
 import numpy as np
-from attorch.dataset import H5SequenceSet
-from scipy.signal import convolve2d
 from tqdm import tqdm
 
 from neuro_data.movies.transforms import Subsequence
@@ -737,118 +735,6 @@ class ScanDataset(dj.Computed):
         treadmills = Treadmill & key
         eyes = (Eye & key) or (Eye2 & key)
         return (responses * treadmills * eyes).proj()
-
-
-class AttributeTransformer:
-    def __init__(self, name, h5_handle, transforms):
-        assert name in h5_handle, '{} must be in {}'.format(name, h5_handle)
-        self.name = name
-        self.h5_handle = h5_handle
-        self.transforms = transforms
-
-    def __getattr__(self, item):
-        if not item in self.h5_handle[self.name]:
-            raise AttributeError('{} is not among the attributes'.format(item))
-        else:
-            ret = self.h5_handle[self.name][item][()]
-            if ret.dtype.char == 'S':  # convert bytes to univcode
-                ret = ret.astype(str)
-            for tr in self.transforms:
-                ret = tr.column_transform(ret)
-            return ret
-
-
-class MovieSet(H5SequenceSet):
-    def __init__(self, filename, *data_keys, transforms=None, stats_source=None):
-        super().__init__(filename, *data_keys, transforms=transforms)
-        self.shuffle_dims = {}
-        self.stats_source = stats_source if stats_source is not None else 'all'
-
-    @property
-    def n_neurons(self):
-        return self[0].responses.shape[1]
-
-    @property
-    def neurons(self):
-        return AttributeTransformer('neurons', self._fid, self.transforms)
-
-    @property
-    def img_shape(self):
-        return (1,) + self[0].inputs.shape
-
-    def transformed_mean(self, stats_source=None):
-        if stats_source is None:
-            stats_source = self.stats_source
-
-        tmp = [np.atleast_1d(self.statistics['{}/{}/mean'.format(dk, stats_source)][()])
-               for dk in self.data_groups]
-        return self.transform(self.data_point(*tmp), exclude=Subsequence)
-
-    def rf_base(self, stats_source='all'):
-        N, c, t, w, h = self.img_shape
-        t = min(t, 150)
-        def mean(dk): return self.statistics['{}/{}/mean'.format(dk, stats_source)][()]
-        d = dict(
-            inputs=np.ones((1, c, t, w, h)) * np.array(mean('inputs')),
-            eye_position=np.ones((1, t, 1)) * mean('eye_position')[None, None, :],
-            behavior=np.ones((1, t, 1)) * mean('behavior')[None, None, :],
-            responses=np.ones((1, t, 1)) * mean('responses')[None, None, :]
-        )
-        return self.transform(self.data_point(*[d[dk] for dk in self.data_groups]), exclude=Subsequence)
-
-    def rf_noise_stim(self, m, t, stats_source='all'):
-        """
-        Generates a Gaussian white noise stimulus filtered with a 3x3 Gaussian filter
-        for the computation of receptive fields. The mean and variance of the Gaussian
-        noise are set to the mean and variance of the stimulus ensemble.
-
-        The behvavior, eye movement statistics, and responses are set to their respective means.
-        Args:
-            m: number of noise samples
-            t: length in time
-
-        Returns: tuple of input, behavior, eye, and response
-
-        """
-        N, c, _, w, h = self.img_shape
-        def stat(dk, what): return self.statistics['{}/{}/{}'.format(dk, stats_source, what)][()]
-        mu, s = stat('inputs', 'mean'), stat('inputs', 'std')
-        h_filt = np.float64([
-            [1 / 16, 1 / 8, 1 / 16],
-            [1 / 8, 1 / 4, 1 / 8],
-            [1 / 16, 1 / 8, 1 / 16]]
-        )
-        noise_input = np.stack([convolve2d(np.random.randn(w, h), h_filt, mode='same')
-                                for _ in range(m * t * c)]).reshape((m, c, t, w, h)) * s + mu
-
-        mean_beh = np.ones((m, t, 1)) * stat('behavior', 'mean')[None, None, :]
-        mean_eye = np.ones((m, t, 1)) * stat('eye_position', 'mean')[None, None, :]
-        mean_resp = np.ones((m, t, 1)) * stat('responses', 'mean')[None, None, :]
-
-        d = dict(
-            inputs=noise_input.astype(np.float32),
-            eye_position=mean_eye.astype(np.float32),
-            behavior=mean_beh.astype(np.float32),
-            responses=mean_resp.astype(np.float32)
-        )
-
-        return self.transform(self.data_point(*[d[dk] for dk in self.data_groups]), exclude=Subsequence)
-
-    def __getitem__(self, item):
-        x = self.data_point(*(np.array(self._fid[g][
-            str(item if g not in self.shuffle_dims else self.shuffle_dims[g][item])])
-            for g in self.data_groups))
-        for tr in self.transforms:
-            x = tr(x)
-        return x
-
-    def __repr__(self):
-        return 'MovieSet m={}:\n\t({})'.format(len(self), ', '.join(self.data_groups)) \
-               + '\n\t[Transforms: ' + '->'.join([repr(tr) for tr in self.transforms]) + ']' \
-               + (
-                   ('\n\t[Shuffled Features: ' + ', '.join(self.shuffle_dims) + ']') if len(
-                       self.shuffle_dims) > 0 else '') + \
-               ('\n\t[Stats source: {}]'.format(self.stats_source) if self.stats_source is not None else '')
 
 
 schema.spawn_missing_classes()
