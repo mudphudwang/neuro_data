72f3bbcd5234d0b5ad2c0c7c68dc30c05eddec56
Eric Wang <ewang@mantle.local>
2020-11-02 20:03:34 -0600

    ddof 0

--- neuro_data/utils/measures.py
+++ neuro_data/utils/measures.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def corr(y1,y2, axis=-1, eps=1e-8, **kwargs):
+def corr(y1,y2, axis=-1, ddof=0, eps=1e-8, **kwargs):
     """
     Compute the correlation between two matrices along certain dimensions. 
 
@@ -8,12 +8,13 @@ def corr(y1,y2, axis=-1, eps=1e-8, **kwargs):
         y1:      first matrix
         y2:      second matrix
         axis:    dimension along which the correlation is computed.
+        ddof:    delta degrees of freedom for standard deviation
         eps:     offset to the standard deviation to make sure the correlation is well defined (default 1e-8)
         **kwargs passed to final mean of standardized y1 * y2
 
     Returns: correlation vector
 
     """
-    y1 = (y1 - y1.mean(axis=axis, keepdims=True))/(y1.std(axis=axis, keepdims=True, ddof=1) + eps)
-    y2 = (y2 - y2.mean(axis=axis, keepdims=True))/(y2.std(axis=axis, keepdims=True, ddof=1) + eps)
+    y1 = (y1 - y1.mean(axis=axis, keepdims=True))/(y1.std(axis=axis, keepdims=True, ddof=ddof) + eps)
+    y2 = (y2 - y2.mean(axis=axis, keepdims=True))/(y2.std(axis=axis, keepdims=True, ddof=ddof) + eps)
     return (y1 * y2).mean(axis=axis, **kwargs)
